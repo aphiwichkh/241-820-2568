e116941345b5248d72d6016993bb16b1a6401d32
aphiwichkh <aphiwich.kh@ku.th>
2026-03-08 13:06:42 +0700

    Add files via upload

--- Frontend/index.js
+++ Frontend/index.js
@@ -1,26 +1,41 @@
-function submitData() {
-    let firsNameDom = document.querySelector('input[name=firstname]');
+const submitData = async () => {
+    let firstNameDom = document.querySelector('input[name=firstname]')
     let lastNameDom = document.querySelector('input[name=lastname]');
     let ageDom = document.querySelector('input[name=age]');
-    
     let genderDom = document.querySelector('input[name=gender]:checked');
-    let interestDoms = document.querySelectorAll('input[name=interest]:checked');
+    let interestDoms = document.querySelectorAll('input[name=interests]:checked');
+    let descriptionDom = document.querySelector('textarea[name=description]');
 
-    let discriptionDom = document.querySelector('textarea[name=description]');
-
-    let interests = ''
+    let messageDOM = document.getElementById('message');
+    try {
+    let interest = ' '
     for (let i = 0; i < interestDoms.length; i++) {
-        interests += interestDoms[i].value
-        if (i != interestDoms.length - 1) {
-            interests += ', '
+        interest += interestDoms[i].value
+        if (i != interestDoms.length -1) {
+            interest += ', '
         }
+    }
+
     let userData = {
-        firstName: firsNameDom.value,
+        firstName: firstNameDom.value,
         lastName: lastNameDom.value,
         age: ageDom.value,
         gender: genderDom.value,
-        discriptionDom: discriptionDom.value,
+        description: descriptionDom.value,
+        interests: interest
     }
-console.log('submit Data', userData);
+   
+        const response = await axios.post('http://localhost:8000/users', userData)
+        console.log('response', response.data);
+
+        messageDOM.innerText = "บันทึกข้อมูลสำเร็จ";
+        messageDOM.className = "message success";
+    } catch (error) {
+        if (error.response) {
+            console.error('Error response from server:', error.response.message);
+        }
+        messageDOM.innerText = "บันทึกข้อมูลไม่สำเร็จ";
+        messageDOM.className = "message danger";
     }
+    
 }
